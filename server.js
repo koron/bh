9c221bc7b63589a818aaba2f3a90b1e08d2d4e2f
MURAOKA Taro <koron.kaoriya@gmail.com>
2012-05-03 09:05:02 +0900

    context object

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 var bh = require('bh');
+var context = new bh.context();
+
 var http = require('http');
+http.createServer(context.handle).listen(8080, '0.0.0.0');
 
-console.log(JSON.stringify(bh));
-
-function handler(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}
-
-http.createServer(handler).listen(8080, '0.0.0.0');
+console.log('Server version ' + bh.version + ' (r' + bh.revision + ') start');
 
 // vim:set ts=8 sts=4 sw=4 tw=0 et:
